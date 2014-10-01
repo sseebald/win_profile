fc39cc502220eb2698262adebb43be635d070103
Spencer <spencer.seebald@puppetlabs.com>
2014-10-01 14:12:10 -0700

    Update windows_baseline.pp
    Removing Exec for disabling the windows firewall. This is now being done in the win_rdp module which is included at the top

--- manifests/windows_baseline.pp
+++ manifests/windows_baseline.pp
@@ -1,6 +1,7 @@
 class profile::windows_baseline {
 
   require registry
+  include win_rdp
   
   reboot { 'before install':
     when => pending,
@@ -13,14 +14,6 @@ class profile::windows_baseline {
 
   class { 'pe_windows_shortcuts':
     user => 'vagrant',
-  }
-
-  #turn off windows firewall
-
-  exec { 'turn off firewall':
-    command  => '$(netsh advfirewall set currentprofile state off)',
-    onlyif   => ' if ((netsh advfirewall show currentprofile | Select-String "State" | Out-String).Replace(" ","").Replace("State","").Replace("`r`n","") = "ON" { exit 0 }',
-    provider => powershell,
   }
 
   package { 'Firefox': }
